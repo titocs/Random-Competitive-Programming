3fce5a5dc387fed539c4aa858cbaeb04782c4e54
Tito Candra Septio <55591161+titocs@users.noreply.github.com>
2021-03-08 09:12:35 +0700

    Create Pak Dengklek dan Operasi Sederhana.cpp

---
+++ Ngoding Seru 2015 - Divisi Pemula/Pak Dengklek dan Operasi Sederhana.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int x, y, jumlah, kurang, kali, bagi, mod;
+    cin >> x >> y;
+    cout << x + y << endl;
+    cout << x - y << endl;
+    cout << x*y << endl;
+    cout << x/y << endl;
+    cout << x % y << endl;
+    return 0;
+}

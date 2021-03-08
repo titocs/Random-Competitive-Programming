ed28f920f7be6eb12d57973259c2f0fd18c30895
Tito Candra Septio <55591161+titocs@users.noreply.github.com>
2021-03-08 08:33:01 +0700

    Update TOKI Idol.cpp

--- Ngoding Seru 2015 - Divisi Pemula/TOKI Idol.cpp
+++ Ngoding Seru 2015 - Divisi Pemula/TOKI Idol.cpp
@@ -3,11 +3,10 @@ using namespace std;
 
 int main(){
     int hasil, x, y, z;
-	  cin >> x >> y >> z;
-	  hasil = x + y + z;
-	  if(hasil > 200)
-		    printf("Lolos");
-	  else
-		    printf("Tidak Lolos");
-	  return 0;
+    cin >> x >> y >> z;
+    if(x + y + z > 200)
+        cout << "Lolos";
+    else
+        cout << "Tidak lolos";
+    return 0;
 }

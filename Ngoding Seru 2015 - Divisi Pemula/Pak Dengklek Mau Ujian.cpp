9e8ccbbc616842cdac03c29635f7d04ca3bb15ad
Tito Candra Septio <55591161+titocs@users.noreply.github.com>
2021-03-08 09:14:15 +0700

    Create Pak Dengklek Mau Ujian.cpp

---
+++ Ngoding Seru 2015 - Divisi Pemula/Pak Dengklek Mau Ujian.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int x;
+    cin >> x;
+    if(x>=0 && x<=100){
+        cout << "YA" << endl;
+    }
+    else
+        cout << "TIDAK" << endl;	
+	return 0;
+}

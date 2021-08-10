062ab759d9d5802501a5f70529194344578596ff
Hfn <55591161+titocs@users.noreply.github.com>
2021-08-10 14:33:33 +0700

    Update Merge - Quick Sort.cpp

--- TOKI/Merge - Quick Sort.cpp
+++ TOKI/Merge - Quick Sort.cpp
@@ -1,17 +1,64 @@
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
 
+void merge(int array[], int const left, int const mid, int const right){
+	auto const subArrayOne = mid - left + 1;
+	auto const subArrayTwo = right - mid;
+
+	auto *leftArray = new int[subArrayOne],
+		*rightArray = new int[subArrayTwo];
+
+	for(auto i = 0; i < subArrayOne; i++)
+		leftArray[i] = array[left + i];
+	for(auto j = 0; j < subArrayTwo; j++)
+		rightArray[j] = array[mid + 1 + j];
+
+	auto indexOfSubArrayOne = 0,
+		indexOfSubArrayTwo = 0;
+	int indexOfMergedArray = left;
+
+	while(indexOfSubArrayOne < subArrayOne && indexOfSubArrayTwo < subArrayTwo){
+		if(leftArray[indexOfSubArrayOne] <= rightArray[indexOfSubArrayTwo]){
+			array[indexOfMergedArray] = leftArray[indexOfSubArrayOne];
+			indexOfSubArrayOne++;
+		}
+		else{
+			array[indexOfMergedArray] = rightArray[indexOfSubArrayTwo];
+			indexOfSubArrayTwo++;
+		}
+		indexOfMergedArray++;
+	}
+	while(indexOfSubArrayOne < subArrayOne) {
+		array[indexOfMergedArray] = leftArray[indexOfSubArrayOne];
+		indexOfSubArrayOne++;
+		indexOfMergedArray++;
+	}
+	while(indexOfSubArrayTwo < subArrayTwo) {
+		array[indexOfMergedArray] = rightArray[indexOfSubArrayTwo];
+		indexOfSubArrayTwo++;
+		indexOfMergedArray++;
+	}
+}
+
+void mergeSort(int array[], int const begin, int const end){
+	if (begin >= end)
+		return;
+
+	auto mid = begin + (end - begin) / 2;
+	mergeSort(array, begin, mid);
+	mergeSort(array, mid + 1, end);
+	merge(array, begin, mid, end);
+}
+
 int main(){
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    int x;
-    cin >> x;
-    int arr[x];
-    for(int i=0; i<x; i++){
+    int n; cin >> n;
+    int arr[n];
+    for(int i=0; i<n; ++i)
         cin >> arr[i];
-    }
-    sort(arr, arr+x);
-    for(int i=0; i<x; i++)
+	auto arr_size = sizeof(arr) / sizeof(arr[0]);
+	mergeSort(arr, 0, arr_size - 1);
+
+	for(int i=0; i<n; ++i)
         cout << arr[i] << endl;
-    return 0;
+	return 0;
 }

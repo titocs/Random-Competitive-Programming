c3227c5de5d641333afc77dd6f5125abf4fdfb28
Hfn <55591161+titocs@users.noreply.github.com>
2021-09-26 23:36:07 +0700

    Update Kode Rahasia.cpp

--- ITBPC 2011 Final Junior/Kode Rahasia.cpp
+++ ITBPC 2011 Final Junior/Kode Rahasia.cpp
@@ -6,7 +6,7 @@ int main(){
     cin >> s >> t;
     sort(s.begin(), s.end());
     sort(t.begin(), t.end());
-    if(s==t)
+    if(s == t)
         cout << "Benar";
     else
         cout << "Salah";

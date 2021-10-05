86cc9b3aa1aedb68d1a8310c69110dfaa1e35f2c
Hfn <55591161+titocs@users.noreply.github.com>
2021-10-05 23:23:10 +0700

    Update Berbalas Pantun.cpp

--- GEMASTIK 2017/Berbalas Pantun.cpp
+++ GEMASTIK 2017/Berbalas Pantun.cpp
@@ -1,14 +1,13 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-typedef long long ll;
 int main(){
-    ll n, sum=0;
+    long long n, sum=0;
     cin >> n;
-    ll a, b;
+    long long a, b;
     for(int i=0; i<n; i++){
         cin >> a >> b;
-        sum+=n*a + n*b;
+        sum + =n*a + n*b;
     }
     cout << sum;
 }

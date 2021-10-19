d0be83fa4e54ec67b23cfb42b92b34dc4befbe39
Hfn <55591161+titocs@users.noreply.github.com>
2021-10-19 14:31:57 +0700

    Update Mex Tahun Baru.cpp

--- TROC #18/Mex Tahun Baru.cpp
+++ TROC #18/Mex Tahun Baru.cpp
@@ -6,16 +6,14 @@ int main(){
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
-    int n;
-    cin >> n;
-    for(int i=1; i<=n; i++){
+    int n; cin >> n;
+    for(int i=1; i<=n; ++i){
         int a;
         cin >> a;
         v.push_back(a);
     }
     sort(v.begin(), v.end());
-    if(n>=3)
-        swap(v[1], v[n-1]);
+    if(n>=3) swap(v[1], v[n-1]);
     for(int a: v){
         cout << a << ' ';
     }

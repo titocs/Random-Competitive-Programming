df31bacb8dfe40f064c833cc732edb281e5bc692
Hfn <55591161+titocs@users.noreply.github.com>
2021-10-20 22:45:37 +0700

    Update Kisah Katak Dalam Sumur.cpp

--- Ngoding Seru Oktober 2015 Divisi Pejuang/Kisah Katak Dalam Sumur.cpp
+++ Ngoding Seru Oktober 2015 Divisi Pejuang/Kisah Katak Dalam Sumur.cpp
@@ -2,15 +2,15 @@
 using namespace std;
 
 int main(){
-    int max = 0, min = 1000000, hasil, x, y, i;
+    int max = 0, min = 1000000, hasil, x, y;
     cin >> x >> y;
     int arr[x];
-    for(i=0; i<x; i++){
+    for(int i=0; i<x; i++){
         cin >> arr[i];
         if(arr[i] > max)
             max = arr[i];
     }
-    for(i=0; i<x; i++){
+    for(int i=0; i<x; i++){
         if(arr[i] < min && max + arr[i] >= y)
             min = arr[i];
     }
